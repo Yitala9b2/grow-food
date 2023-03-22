232033753c29adc0fdbd419f9ad51db8a8515141
Виталий <94127565+Yitala9b2@users.noreply.github.com>
2023-03-22 21:16:54 +0300

    localStorage теперь принимает и начальные значения

--- js/script.js
+++ js/script.js
@@ -1,98 +1,3 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = { };
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/main.js");
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ "./src/js/main.js":
-/*!************************!*\
-  !*** ./src/js/main.js ***!
-  \************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
 window.addEventListener('DOMContentLoaded', () => {
   // табы
   const tabs = document.querySelectorAll('.tabheader__item');
@@ -695,22 +600,35 @@ if (slides.length < 10) {
 
     // КАЛЬКУЛЯТОР
     const result = document.querySelector('.calculating__result span');
-
-
-
     let sex, height, weight, age, ratio;
 
-    function sexNRatio(atr, value, item) {
-        if (localStorage.getItem(atr)) {
-            item = localStorage.getItem(value);
-        }else{
-            item = value
-            localStorage.setItem(atr, value);
-        }
-    };
-    sexNRatio('sex', 'female', sex);
-    sexNRatio('ratio', 1.375, ratio);
 
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    }else{
+        sex = 'female'
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    }else{
+        ratio = 1.375
+        localStorage.setItem('ratio', 1.375);
+    }
+
+
+    //function sexNRatio(atr, value, item) {
+    //    if (localStorage.getItem(atr)) {
+    //        item = localStorage.getItem(value);
+    //    }else{
+    //        item = value
+    //        localStorage.setItem(atr, value);
+    //    }
+    //};
+    //sexNRatio('sex', 'female', sex);
+    //sexNRatio('ratio', 1.375, ratio);
+    
     function initLocalSettings(selector, activeClass) {
         const elements = document.querySelectorAll(selector);
         elements.forEach((elem) => {
@@ -725,7 +643,7 @@ if (slides.length < 10) {
     }
     initLocalSettings('#gender div', 'calculating__choose-item_active');
     initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
-
+    
     function calcTotal() {
         if (!sex || !height || !weight || !age || !ratio) {
             result.textContent = '____';
@@ -736,14 +654,12 @@ if (slides.length < 10) {
         } else {
             result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
         }
-      
     }
 
     calcTotal()
 
     const getStaticInformation = (selector, activeClass) => {
         const elements = document.querySelectorAll(selector);
-        console.log(elements)
         elements.forEach((elem) => (
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio') ) {
@@ -793,8 +709,3 @@ if (slides.length < 10) {
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
 });
-
-/***/ })
-
-/******/ });
-//# sourceMappingURL=script.js.map
